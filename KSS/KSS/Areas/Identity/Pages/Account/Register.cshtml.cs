fe2bd9c944e2464b9e6c91f7d51274be1921ec38
Spencer Shaw <w0413731@selu.edu>
2018-08-21 20:19:59 -0500

    Migrated Stuff. Added TODO's

--- KSS/KSS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ KSS/KSS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using KSS.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -66,7 +67,7 @@ namespace KSS.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new KSSUser { UserName = Input.Email, Email = Input.Email  };//TODO Add FirstName Last Name and Cell
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
