98e840a73ff3e615735f7bb85ab0bbec88bfc0f6
Donny <dvicknair@charter.net>
2018-08-24 18:15:41 -0500

    Added migration for locations, added location menu link, and changed ensurecreated to migrate (this needs more attention so nothing runs if not needed).

--- KSS/KSS/Data/Migrations/20180824230930_locationTable.Designer.cs
+++ KSS/KSS/Data/Migrations/20180824230930_locationTable.Designer.cs
@@ -4,14 +4,16 @@ using KSS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KSS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180824230930_locationTable")]
+    partial class locationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
