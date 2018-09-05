4471ab52e9dc6c8b2eb0a36c244b570ebc4390b2
Weston Arnold <weston.arnold>
2018-09-04 19:19:02 -0500

    Added remaining API controllers
    Added Course, Enrollment, and Instance API controllers and verified the project can still build and
    Swagger is able to access them all.

---
+++ KSS/KSS/Areas/CoursesApiController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KSS.Data;
+using KSS.Models;
+
+namespace KSS.Areas
+{
+    [Route("api/Course")]
+    [ApiController]
+    public class CoursesApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CoursesApi
+        [HttpGet]
+        public IEnumerable<Course> GetCourse()
+        {
+            return _context.Course;
+        }
+
+        // GET: api/CoursesApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var course = await _context.Course.FindAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        // PUT: api/CoursesApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse([FromRoute] int id, [FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.CourseId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CoursesApi
+        [HttpPost]
+        public async Task<IActionResult> PostCourse([FromBody] Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Course.Add(course);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCourse", new { id = course.CourseId }, course);
+        }
+
+        // DELETE: api/CoursesApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            _context.Course.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return Ok(course);
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Course.Any(e => e.CourseId == id);
+        }
+    }
+}
---
+++ KSS/KSS/Areas/EnrollmentApiController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KSS.Data;
+using KSS.Models;
+
+namespace KSS.Areas
+{
+    [Route("api/Enrollment")]
+    [ApiController]
+    public class EnrollmentApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnrollmentApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EnrollmentApi
+        [HttpGet]
+        public IEnumerable<Enrollment> GetEnrollment()
+        {
+            return _context.Enrollment;
+        }
+
+        // GET: api/EnrollmentApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEnrollment([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var enrollment = await _context.Enrollment.FindAsync(id);
+
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(enrollment);
+        }
+
+        // PUT: api/EnrollmentApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEnrollment([FromRoute] int id, [FromBody] Enrollment enrollment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != enrollment.EnrollmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(enrollment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EnrollmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EnrollmentApi
+        [HttpPost]
+        public async Task<IActionResult> PostEnrollment([FromBody] Enrollment enrollment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Enrollment.Add(enrollment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEnrollment", new { id = enrollment.EnrollmentId }, enrollment);
+        }
+
+        // DELETE: api/EnrollmentApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEnrollment([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var enrollment = await _context.Enrollment.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Enrollment.Remove(enrollment);
+            await _context.SaveChangesAsync();
+
+            return Ok(enrollment);
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+            return _context.Enrollment.Any(e => e.EnrollmentId == id);
+        }
+    }
+}
