a6123fe6232987b1a8e279f06d33c7a492438eca
Donald Vicknair <dvicknair@charter.net>
2018-09-23 18:47:14 -0500

    Added Course create and edit viewmodels, added validation and updated controller, and added None / null option for course prereq.

---
+++ KSS/KSS/Areas/Admin/ViewModels/CourseCreate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KSS.Areas.Admin.ViewModels
+{
+    public class CourseCreate
+    {
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Name min length of 6, max Length of 30")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TypicalPrice { get; set; }
+        public bool BookAvailable { get; set; }
+        public decimal BookPrice { get; set; }
+        public int? PrereqId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ KSS/KSS/Areas/Admin/ViewModels/CourseEdit.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KSS.Areas.Admin.ViewModels
+{
+    public class CourseEdit
+    {
+        public int CourseId { get; set; }
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Name min length of 6, max Length of 30")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TypicalPrice { get; set; }
+        public bool BookAvailable { get; set; }
+        public decimal BookPrice { get; set; }
+        public int? PrereqId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
