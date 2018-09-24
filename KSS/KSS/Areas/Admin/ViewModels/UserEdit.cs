9f16668d23298801bb6924e9ad6e7619c99a88df
Weston A <tallonxvii@gmail.com>
2018-09-24 10:59:57 -0500

    Created UserEdit ViewModel

---
+++ KSS/KSS/Areas/Admin/ViewModels/UserEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSS.Areas.Admin.ViewModels
+{
+    public class UserEdit
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public int StaffId { get; set; }
+    }
+}
