a136418d6b01b886ee8964c96600ddf2796056ce
SaraRoberts <sara.roberts@selu.edu>
2018-10-04 18:23:07 -0500

    Temporary Navbar

--- KSS/KSS/student-app/src/components/NavMenu.js
+++ KSS/KSS/student-app/src/components/NavMenu.js
@@ -1,15 +1,26 @@
 ﻿import React, { Component } from 'react';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import './styles/NavMenu.css';
+//import './styles/NavMenu.css';
 
 export class NavMenu extends Component {
     displayName = NavMenu.name
 
     render() {
         return (
-            <h1> This will be the navbar </h1>
+            <ul>
+            <li><a href="/">Home  </a></li>
+                <li><a href="/group">Group  </a></li>
+                <li><a href="/catalog">Catalog  </a></li>
+                <li><a href="/contact">Contact  </a></li>
+                <li><a href="/login">Login  </a></li>
+                <li><a href="/logout">Logout  </a></li>
+                <li><a href="/register">Register  </a></li>
+                <li> <a href="/myclasses">MyClasses  </a></li >
+                <li><a href="/account">Account  </a></li >
+                <li><a href="/search">Search  </a></li >
+            </ul >
         );
     }
 }
