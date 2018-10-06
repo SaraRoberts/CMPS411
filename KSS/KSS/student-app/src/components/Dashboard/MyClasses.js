4ac5e4466774dc56e1f3b5a9898556792d999561
SaraRoberts <sara.roberts@selu.edu>
2018-10-05 19:49:43 -0500

    Example of pulling from the API's (APIs need adjustments)

--- KSS/KSS/student-app/src/components/Dashboard/MyClasses.js
+++ KSS/KSS/student-app/src/components/Dashboard/MyClasses.js
@@ -1,14 +1,64 @@
 ﻿import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Table } from 'semantic-ui-react';
 
 export class MyClasses extends Component {
     displayName = MyClasses.name
 
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { instances: [], loading: true };
+
+        fetch('/api/instances', { credentials: 'same-origin' })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ instances: data, loading: false });
+            });
+    }
+
+    static renderinstanceTable(instances) {
         return (
-            <h1> MyClasses </h1>
+
+            <Table color={'grey'} inverted inverted>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Course</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                        <Table.HeaderCell>Start Date</Table.HeaderCell>
+                        <Table.HeaderCell>Location</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {instances.map(instances =>
+                        <Table.Row key={instances.instanceId}>
+                            <Table.Cell>{instances.courseId}</Table.Cell>
+                            <Table.Cell>{instances.seats}</Table.Cell>
+                            <Table.Cell>{instances.startDate}</Table.Cell>
+                            <Table.Cell>{instances.locationId}</Table.Cell>
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : MyClasses.renderinstanceTable(this.state.instances);
+
+        return (
+            <div class="grid-container-pages">
+                <div class="grid-item-pages">
+                    <h1>Upcoming Classes</h1>
+                </div>
+                <div class="grid-item-pages">
+                    {contents}
+                </div>
+            </div>
         );
     }
 }
