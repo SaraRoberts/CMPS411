29ac49cdefb826ceba1d7a3e81960a461d785905
Weston A <tallonxvii@gmail.com>
2018-10-16 19:00:54 -0500

    Started styling... Finally
    Set up styling components for .js side of project

--- KSS/KSS/student-app/src/components/Home.js
+++ KSS/KSS/student-app/src/components/Home.js
@@ -1,16 +1,16 @@
 ﻿import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import home1 from'./images/home1.jpg';
+import home1 from './images/home1.jpg';
+import './styles/HomePage.css';
 
 export class Home extends Component {
     displayName = Home.name
-
     render() {
         return (
             <div>
-                <h1> Home </h1>
-                <img src={home1} alt="Home1" />
+                <img className="topImage" src={home1} alt="Home1" />
+                <div></div>
             </div>
         );
     }
