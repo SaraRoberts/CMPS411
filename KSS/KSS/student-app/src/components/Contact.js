6fd903525f78393b9b93dc1624a1c5b8383242b8
SaraRoberts <sara.roberts@selu.edu>
2018-10-23 20:12:56 -0500

    Reading correct fields from API

--- KSS/KSS/student-app/src/components/Contact.js
+++ KSS/KSS/student-app/src/components/Contact.js
@@ -14,7 +14,13 @@ export class Contact extends Component {
                     <h2> About Us </h2>
                         <p> Keeping Safety Smart was founded October 17th, 2018 by Beckey Smith. Etc.</p> 
                     <h2> Mission Statement</h2>
-                        <p> Teaching life saving tools to all. Placeholder. </p>
+                    <p> 
+                    Keeping Safety Smart LLC. is my company and it is an honor and privilege to help educate. The company cornerstone is to 
+                    bring the strongest safety education to our community.  We are a program with the Louisiana Bureau of Emergency Medical Services 
+                    as well as a national registry company, which means we have met all State of Louisiana requirements including a Medical Director 
+                    to teach of our courses. We offer over 25 years of safety education experience.  Please feel free to look at our courses and we 
+                    would enjoy the opportunity to help you further your education. 
+                    </p>
                     <h2> Contact</h2>
                         <p>
                         Beckey Smith, Owner <br></br>
