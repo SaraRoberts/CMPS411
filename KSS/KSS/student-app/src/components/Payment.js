0a077b2b03df43e2ba7d49f4473892a81edbff8a
Weston A <tallonxvii@gmail.com>
2018-10-30 11:30:52 -0500

    Added paypal button with payment sandbox functional. Production needs fixing

--- KSS/KSS/student-app/src/components/Payment.js
+++ KSS/KSS/student-app/src/components/Payment.js
@@ -3,17 +3,20 @@ import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PaypalExpressBtn from 'react-paypal-express-checkout';
 
-export class Payment extends React.Component {
+export class Payment extends Component {
     render() {
         const client = {
+            //sandbox and client IDs, different based on the action you are trying to perform
             sandbox: 'AU7-TZCTcPacLNP7bj74quFlQPLWzG9jlMB8Zqr5m4wnygK-ckrcuV6izspeNmb-su0VXrXWSsjXZAY-',
             production: 'ATt_35Hio2zgeOr0HRxARGxst3ewohkZXzSJ7N4Ds3kwynkon66oriV6zuxLq5RfYW5l64d0dQJoBTAB',
         }
         return (
             <PaypalExpressBtn
+                env={'live'} //change this to 'live' to complete REAL transactions
                 client={client}
                 currency={'USD'}
-                total={0.01} />
+                total={0.01}
+            />
         );
     }
 } 
