3f382a212cd7d8cc34df9921b68278705d6f8eda
Weston A <tallonxvii@gmail.com>
2018-10-30 11:58:14 -0500

    [BUG FIX] Fixed production not working and added result methods based on outputs

--- KSS/KSS/student-app/src/components/Payment.js
+++ KSS/KSS/student-app/src/components/Payment.js
@@ -3,19 +3,35 @@ import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PaypalExpressBtn from 'react-paypal-express-checkout';
 
-export class Payment extends Component {
+export class Payment extends React.Component {
+    displayName = Payment.name
     render() {
+        const onSuccess = (payment) => {
+            console.log("Successful: ", payment);
+        };
+
+        const onError = (err) => {
+            console.log("Failure: ", err);
+        };
+
+        const onCancel = (data) => {
+            console.log('Canceled', data);
+        };
+
         const client = {
             //sandbox and client IDs, different based on the action you are trying to perform
             sandbox: 'AU7-TZCTcPacLNP7bj74quFlQPLWzG9jlMB8Zqr5m4wnygK-ckrcuV6izspeNmb-su0VXrXWSsjXZAY-',
-            production: 'ATt_35Hio2zgeOr0HRxARGxst3ewohkZXzSJ7N4Ds3kwynkon66oriV6zuxLq5RfYW5l64d0dQJoBTAB',
-        }
+            production: 'ATt_35Hio2zgeOr0HRxARGxst3ewohkZXzSJ7N4Ds3kwynkon66oriV6zuxLq5RfYW5l64d0dQJoBTAB'
+        };
         return (
             <PaypalExpressBtn
-                env={'sandbox'} //change this to 'live' to complete REAL transactions
+                env={'production'} //change this to 'live' to complete REAL transactions
                 client={client}
                 currency={'USD'}
                 total={0.01}
+                onSuccess={onSuccess}
+                onError={onError}
+                onCancel={onCancel}
             />
         );
     }
