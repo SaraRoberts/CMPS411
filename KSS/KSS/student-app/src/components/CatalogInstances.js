359f5c5fd39ad8fcf4148982d06901587ec07af0
Donald Vicknair <dvicknair@charter.net>
2018-11-17 18:50:36 -0600

    Created book now modal containing paypal button with specific instance data.

--- KSS/KSS/student-app/src/components/CatalogInstances.js
+++ KSS/KSS/student-app/src/components/CatalogInstances.js
@@ -4,6 +4,7 @@ import { Button, Table } from 'semantic-ui-react';
 import './styles/Tables.css';
 import PaypalExpressBtn from 'react-paypal-express-checkout';
 import Modal from 'react-modal';
+import { InstanceModal } from './InstanceModal'
 const customStyles = {
     content: {
         top: '50%',
@@ -23,7 +24,15 @@ export class CatalogInstances extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { instances: [], loading: true, courseInfo: null, modalIsOpen: false   };
+        this.state = {
+            instances: [],
+            courseInfo: null,
+            showR: false,
+            modalInstance: {
+                id: "",
+                price: ""
+            }
+        };
 
         console.log(this.props);
         console.log(this.props.match.params.courseId);
@@ -42,95 +51,20 @@ export class CatalogInstances extends Component {
                 this.setState({ courseInfo: data2, loading: false });
                 console.log(data2);
             });
-        this.openModal = this.openModal.bind(this);
-        this.afterOpenModal = this.afterOpenModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
     }
 
-    openModal() {
-        this.setState({ modalIsOpen: true });
+    showModalR = (passedid, passedprice) => {
+        this.setState({
+            ...this.state,
+            modalInstance: {
+                id: passedid,
+                price: passedprice
+            },
+            showR: !this.state.showR,
+        })
     }
 
-    afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        this.subtitle.style.color = '#f00';
-    }
-
-    closeModal() {
-        this.setState({ modalIsOpen: false });
-    }
-
-    static rendercourseTable(instances) {
-        const onSuccess = (payment) => {
-            console.log("Successful: ", payment);
-        };
-
-        //Output in browser console on cancel (closing the window)
-        const onCancel = (data) => {
-            console.log('Canceled', data);
-        };
-
-        //If an error is encountered in the API or it crashes
-        const onError = (err) => {
-            console.log("Failure: ", err);
-        };
-
-        const client = {
-            //For testing, use sandbox, for live, use Production. The 'env' variable will also need to be changed in the PayPalExpressBtn element below
-            sandbox: 'AU7-TZCTcPacLNP7bj74quFlQPLWzG9jlMB8Zqr5m4wnygK-ckrcuV6izspeNmb-su0VXrXWSsjXZAY-',
-            production: 'ATt_35Hio2zgeOr0HRxARGxst3ewohkZXzSJ7N4Ds3kwynkon66oriV6zuxLq5RfYW5l64d0dQJoBTAB'
-        };
-
-        return (
-            <Table>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Start Date</Table.HeaderCell>
-                        <Table.HeaderCell>Location</Table.HeaderCell>
-                        <Table.HeaderCell>Details</Table.HeaderCell>
-                        <Table.HeaderCell></Table.HeaderCell>
-                        <Table.HeaderCell></Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {instances.map(instances =>
-                        <Table.Row key={instances.instanceId}>
-                            <Table.Cell>{instances.startDate}</Table.Cell>
-                            <Table.Cell>{instances.locationName}<br />
-                                        {instances.locationStreet} <br />
-                                        {instances.locationCity}, {instances.locationState} {instances.locationZip}</Table.Cell>
-                            <Table.Cell>Instructor: {instances.instructorName}<br />
-                                        Price: ${instances.price}.00<br />
-                                        Capacity: {instances.seats}</Table.Cell>
-                            <Table.Cell>
-                                <Button className="details-button" >See Details</Button>
-                                <PaypalExpressBtn
-                                    env={'sandbox'} //change this to 'production' to complete REAL transactions
-                                    client={client}
-                                    currency={'USD'}
-
-                                    total={instances.price} //change this to adjust price
-
-                                    //output handlers
-                                    onSuccess={onSuccess}
-                                    onError={onError}
-                                    onCancel={onCancel}
-                                />
-                            </Table.Cell>
-                            <Table.Cell>
-                               
-                            </Table.Cell>
-                        </Table.Row>
-                        )}
-                </Table.Body>
-            </Table>
-        );
-    }
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : CatalogInstances.rendercourseTable(this.state.instances);
-
         return (
             <div id="page">
                 <div id="main-container">
@@ -145,29 +79,50 @@ export class CatalogInstances extends Component {
                         } 
                         </div>
                         <div class="grid-item-pages">
-                            {contents}
+                            <Table>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>Start Date</Table.HeaderCell>
+                                        <Table.HeaderCell>Location</Table.HeaderCell>
+                                        <Table.HeaderCell>Details</Table.HeaderCell>
+                                        <Table.HeaderCell></Table.HeaderCell>
+                                        <Table.HeaderCell></Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {this.state.instances.map(instances =>
+                                        <Table.Row key={instances.instanceId}>
+                                            <Table.Cell>{instances.startDate}</Table.Cell>
+                                            <Table.Cell>{instances.locationName}<br />
+                                                {instances.locationStreet} <br />
+                                                {instances.locationCity}, {instances.locationState} {instances.locationZip}</Table.Cell>
+                                            <Table.Cell>Instructor: {instances.instructorName}<br />
+                                                Price: ${instances.price}.00<br />
+                                                Capacity: {instances.seats}</Table.Cell>
+                                            <Table.Cell>
+                                                <button
+                                                    onClick={
+                                                        () => this.showModalR(
+                                                            instances.instanceId,
+                                                            instances.price
+                                                        )
+                                                    }
+                                                >Book Now!</button>
+
+                                            </Table.Cell>
+                                        </Table.Row>
+                                    )}
+                                </Table.Body>
+                            </Table>
                         </div>
                     </div>
-                    <button onClick={this.openModal}>Open Modal</button>
-                    <Modal
-                        isOpen={this.state.modalIsOpen}
-                        onAfterOpen={this.afterOpenModal}
-                        onRequestClose={this.closeModal}
-                        style={customStyles}
-                        contentLabel="Example Modal"
-                    >
 
-                        <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-                        <button onClick={this.closeModal}>close</button>
-                        <div>I am a modal</div>
-                        <form>
-                            <input />
-                            <button>tab navigation</button>
-                            <button>stays</button>
-                            <button>inside</button>
-                            <button>the modal</button>
-                        </form>
-                    </Modal>
+                    <InstanceModal
+                        show={this.state.showR}
+                        onClose={this.showModalR}
+                        modalInstance={this.state.modalInstance}
+                    />
+                    
                 </div>
             </div>
         );
---
+++ KSS/KSS/student-app/src/components/InstanceModal.js
@@ -0,0 +1,64 @@
+﻿import React, { Component } from 'react';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+import './styles/InstanceModal.css';
+
+
+export class InstanceModal extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    onClose = (e) => {
+        this.props.onClose && this.props.onClose(e)
+    }
+
+
+    render() {
+        if (!this.props.show) return null
+        const onSuccess = (payment) => {
+            console.log("Successful: ", payment);
+        };
+
+        //Output in browser console on cancel (closing the window)
+        const onCancel = (data) => {
+                console.log('Canceled', data);
+        };
+
+        //If an error is encountered in the API or it crashes
+        const onError = (err) => {
+                console.log("Failure: ", err);
+        };
+
+        const client = {
+                //For testing, use sandbox, for live, use Production. The 'env' variable will also need to be changed in the PayPalExpressBtn element below
+                sandbox: 'AU7-TZCTcPacLNP7bj74quFlQPLWzG9jlMB8Zqr5m4wnygK-ckrcuV6izspeNmb-su0VXrXWSsjXZAY-',
+                production: 'ATt_35Hio2zgeOr0HRxARGxst3ewohkZXzSJ7N4Ds3kwynkon66oriV6zuxLq5RfYW5l64d0dQJoBTAB'
+        };
+
+        return (
+            <div id="modalBack">
+                <div id="modal-left"></div>
+                <div id="modal-right">
+                    <input type="button" className="close btn-link"
+                        value="Cancel"
+                        onClick={(e) => { this.onClose(e) }}
+                    />
+                    <PaypalExpressBtn
+                        env={'sandbox'} //change this to 'production' to complete REAL transactions
+                        client={client}
+                        currency={'USD'}
+
+                        total={this.props.modalInstance.price} //change this to adjust price
+
+                        //output handlers
+                        onSuccess={onSuccess}
+                        onError={onError}
+                        onCancel={onCancel}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
