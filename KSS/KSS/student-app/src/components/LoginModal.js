3220dd2709501fd3b03f49181a02909a9882b119
Weston A <tallonxvii@gmail.com>
2018-11-20 19:32:28 -0600

    Added modal to home page for login

---
+++ KSS/KSS/student-app/src/components/LoginModal.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+import './styles/InstanceModal.css';
+
+export class LoginModal extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onClose = (e) => {
+        this.props.onClose && this.props.onClose(e)
+    }
+
+    render() {
+        if (!this.props.show) return null
+
+        return (
+            <div id="modalBack">
+                    <div id="modal-left"></div>
+                    <div id="modal-right" onClick="">
+                        <span id="closeButton" onClick={(e) => { this.onClose(e) }} onMouseOver="">
+                            X
+                            </span>
+                    <h5>or <a href="/">Book and Pay Later</a></h5>
+                </div>
+            </div>
+        );
+    }
+}
