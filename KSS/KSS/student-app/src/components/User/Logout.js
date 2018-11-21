4626400d20fac897fc205743c324d32c8c8be325
Donald Vicknair <dvicknair@charter.net>
2018-11-21 17:45:26 -0600

    Fixed issue where login doesn't work just after logout.

--- KSS/KSS/student-app/src/components/User/Logout.js
+++ KSS/KSS/student-app/src/components/User/Logout.js
@@ -1,46 +1,15 @@
 ﻿import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import { withRouter } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export class Logout extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            user: ''
-        };
-        this.handleLogout = this.handleLogout.bind(this);
-    }
-
+    
     handleLogout() {
-        axios.post('api/account/logout')
-        this.handleHome();
+        fetch('api/account/logout', { method: 'POST' });
+        window.location.href = '/login';
     }
 
-    handleHome() {
-        console.log(this.props)
-        //this.props.history.push('/login')
-        window.location.href = '/login/'
-    }
     render() {
-        //console.log("render");
-        //console.log(this.props.user);
-        if (this.props.user == 0) {
-            return (
-                <div>You are not logged in</div>
-    )
+        return (
+            this.handleLogout()
+        )
+    }
 }
-else {
-    return (
-        this.handleLogout()
-        //<div>
-        //    <button onClick={this.handleLogout}> Logout </button>
-        //</div>
-    )
-}
-}
-}
-export default Logout;
