d55413d48e8af27b01ad2a72e285e6d43944c277
Donald Vicknair <dvicknair@charter.net>
2018-11-22 18:57:41 -0600

    Set global main container media max-width and refactored login to account for global main container.

--- KSS/KSS/student-app/src/components/User/Login.js
+++ KSS/KSS/student-app/src/components/User/Login.js
@@ -58,40 +58,29 @@ export class Login extends Component {
 
     render() {
         return (
-            <div class="container">
-                <div class="row main">
-                    <div class="panel-heading">
-                        <div class="panel-title text-center">
-                            <h3 class="title">Login</h3>
+            <div>
+                <h2>Login</h2>
+                <div className="main-login main-center">
+                    <form onSubmit={this.handleSubmit} method="post">
+                        <div>
+                            <label>Email</label>
+                            <div>
+                                <div>
+                                    <input type="text" className="form-blank" name="email" id="email" placeholder="Enter your Email" onChange={this.handleChange} />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="main-login main-center">
-                        <form onSubmit={this.handleSubmit} class="form-horizontal" method="post">
-                            <div class="form-group">
-                                <label for="email" class="cols-sm-2 control-label">Email</label>
-                                <div class="cols-sm-10">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                        <input type="text" class="form-control" class="form-blank" name="email" id="email" placeholder="Enter your Email" onChange={this.handleChange} />
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div class="form-group">
-                                <label for="password" class="cols-sm-2 control-label">Password</label>
-                                <div class="cols-sm-10">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                        <input type="password" class="form-control" class="form-blank" name="password" id="password" placeholder="Enter your Password" onChange={this.handleChange} />
-                                    </div>
-                                </div>
+                        <div>
+                            <label>Password</label>
+                            <div>
+                                <input type="password" className="form-blank" name="password" id="password" placeholder="Enter your Password" onChange={this.handleChange} />
                             </div>
-
-                            <div class="form-group ">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block login-button">Login</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div>
+                            <button type="submit" className="login-button">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
