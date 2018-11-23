2a629fc2e28391a38895f769451085ecf5f0e74f
Donald Vicknair <dvicknair@charter.net>
2018-11-22 20:49:30 -0600

    Refactored Catalog component using react best practices.

--- KSS/KSS/student-app/src/components/Catalog.js
+++ KSS/KSS/student-app/src/components/Catalog.js
@@ -5,18 +5,6 @@ import './styles/Tables.css';
 
 
 export class Catalog extends Component {
-    displayName = Catalog.name
-
-    constructor(props) {
-        super(props);
-        this.state = { courses: [], loading: true };
-
-        fetch('/api/courses', { credentials: 'same-origin' })
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ courses: data, loading: false });
-            });
-    }
 
     static rendercourseTable(courses) {
       return (
@@ -25,27 +13,43 @@ export class Catalog extends Component {
                 <Table.Row>
                     <Table.HeaderCell>Course</Table.HeaderCell>
                     <Table.HeaderCell>Description</Table.HeaderCell>
-                    <Table.HeaderCell></Table.HeaderCell>
+                    <Table.HeaderCell />
                 </Table.Row>
             </Table.Header>
 
             <Table.Body>
-            {courses.map(courses =>
-                <Table.Row key={courses.courseId}>
-                    <Table.Cell>{courses.name}</Table.Cell>
-                    <Table.Cell>{courses.description}</Table.Cell>
-                    <Table.Cell>
-                        <Link to={`catalog/${courses.courseId}`}>
-                            <button className="redButton">
-                                Upcoming Classes
+                {courses.map(courses => (
+                        <Table.Row key={courses.courseId}>
+                            <Table.Cell>{courses.name}</Table.Cell>
+                            <Table.Cell>{courses.description}</Table.Cell>
+                            <Table.Cell>
+                                <Link to={`catalog/${courses.courseId}`}>
+                                    <button className="redButton">
+                                        Upcoming Classes
                             </button>
-                        </Link>
-                    </Table.Cell>
-                </Table.Row>
+                                </Link>
+                            </Table.Cell>
+                        </Table.Row>
+                    )
                 )}
             </Table.Body>
         </Table>
         );
+    }
+
+    displayName = Catalog.name
+
+    constructor(props) {
+        super(props);
+        this.state = { courses: [], loading: true };
+    }
+
+    componentDidMount() {
+        fetch('/api/courses', { credentials: 'same-origin' })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ courses: data, loading: false });
+            });
     }
 
     render() {
@@ -53,23 +57,17 @@ export class Catalog extends Component {
             ? <p><em>Loading...</em></p>
             : Catalog.rendercourseTable(this.state.courses);
 
-    return (
-        <div id="page">
-            <div id="main-container">
-                <div class="grid-container-pages">
-                    <div class="grid-item-pages">
-                        <h1>Course Catalog</h1>
-                        <p>
-                            Keeping Safety Smart offers a variety of courses that teach valuable first responder skills. Search through
-                            course listings below.
-                        </p>
-                    </div>
-                    <div class="grid-item-pages">
-                        {contents}
-                    </div>
+        return (
+            <div>
+                <div>
+                    <h1>Course Catalog</h1>
+                    <p>
+                        Keeping Safety Smart offers a variety of courses that teach valuable first responder skills. Search through
+                        course listings below.
+                    </p>
                 </div>
+                {contents}
             </div>
-        </div>
         );
     }
 }
