59a96245ba32796f65444d2f6fa714db3d13ef4a
SaraRoberts <sara.roberts@selu.edu>
2018-12-26 14:37:50 -0600

    Creating Car Seat category solution

--- KSS/KSS/student-app/src/App.js
+++ KSS/KSS/student-app/src/App.js
@@ -12,6 +12,7 @@ import { Logout } from './components/User/Logout';
 import { Register } from './components/User/Register';
 import { Staff } from './components/Staff';
 import { StudentDashboard } from './components/User/StudentDashboard';
+import { CarSeatSafety } from './components/CarSeatSafety';
 import 'toastr/build/toastr.min.css';
 
 class App extends Component {
@@ -66,6 +67,7 @@ class App extends Component {
                 <Route path='/login' render={(props) => <Login {...props} loginState={this.state.loginState} />} />
                 <Route path='/logout' render={(props) => <Logout {...props} loginState={this.state.loginState} />} />
                 <Route path='/register' render={(props) => <Register {...props} loginState={this.state.loginState} />} />
+                <Route path='/carseatsafety' render={(props) => <CarSeatSafety {...props} loginState={this.state.loginState} />} />
                 <Route path='/dashboard' render={(props) => <StudentDashboard {...props} loginState={this.state.loginState} />} />
             </Layout>
         );
---
+++ KSS/KSS/student-app/src/components/CarSeatSafety.js
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import car from './images/car.jpg';
+import initialsLogo from './images/initialsLogo.png';
+import './styles/GroupPage.css';
+
+export class CarSeatSafety extends Component {
+    displayName = CarSeatSafety.name
+
+    componentDidMount() {
+        document.title = "Keeping Safety Smart | Car Seat Safety";
+    }
+
+    render() {
+        return (
+            <div>
+            <div id="groupBanner" alt="Group Safety Classes" />
+
+            <div className="groupInfo">
+            <div>
+            <h2>Car Seat Safety</h2>
+            <p>
+                We offer assistance with car seat installation and safety free of charge to make sure
+                your little ones ride safely. Please contact Nicole Dufrene for more information!
+            </p>
+            <p>
+                Nicole Dufrene, EMR <br />
+                nicoledufrene0301@gmail.com<br></br>
+            </p>
+    </div>
+        </div>
+                <div className="carInfo">
+                    <br /><br /><br /><br /><br />
+                <img src={car} alt="Group Safety Classes image" />
+            </div>
+        </div>
+        );
+    }
+}
